6433e7d0aa81252dceb9cfbb05f79ec9e5b030eb
veda1612 <vedavasavisiri@gmail.com>
2021-01-19 03:43:20 +0000

    add express_server.js

--- express_server.js
+++ express_server.js
@@ -20,6 +20,16 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:"card-title" };
+  res.render("urls_show", templateVars);
+});
 /*
 app.get("/set", (req, res) => {
     const a = 1;
